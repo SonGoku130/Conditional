1e7c8ffb8f799c5ff8495ff670535056e2bba7fb
SonGoku130 <aaryan2221t@gmail.com>
2026-06-17 16:53:47 +0530

    Add files via upload

---
+++ sales.py
@@ -0,0 +1,8 @@
+actual = float(input("Please list the amount you got the item for : "))
+sales = float(input("Please list the amount you sold the item for : "))
+
+if sale > actual:
+    amount = sale - actual
+    print("Profit of = ", amount)
+else
+print("No prOfiT!!!")
